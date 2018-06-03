6db829f2c5d7bf8bf1024236aa7ed78bc4633173
wolskippp <w.piotr94@gmail.com>
2018-06-04 00:48:52 +0200

    added restrictions to pixel value also added requirements.txt

--- src/FakeImgCandidate.py
+++ src/FakeImgCandidate.py
@@ -1,3 +1,7 @@
+
+import numpy as np
+
+
 class FakeImgCandidate(object):
     def __init__(self, img):
         self.img = img
@@ -8,3 +12,7 @@ class FakeImgCandidate(object):
 
     def get_pixel_value(self, x, y):
         return self.img[x, y]
+
+    def clip_all_image(self, image_min_values, image_max_values):
+        self.img = np.clip(self.img, image_min_values, image_max_values)
+        self.img = np.clip(self.img, -1.0, 1.0)
--- src/main.py
+++ src/main.py
@@ -7,17 +7,17 @@ if __name__ == '__main__':
     # class_name_to_fake = "toy_poodle"
     img_path = Utils.get_test_image_path(img_name)
 
-    p_max = 0.1 # max 1
-    p_min = -0.1 # min -1
-    fake_class_prob_to_get = 50
+    p_max = 0.05 # max 1
+    p_min = -0.05 # min -1
+    fake_class_prob_to_get = 30
     fake_img_generator = FakeImgGenerator(img_path,
                                           p_max,
                                           p_min,
                                           fake_class_prob_to_get
                                           )
 
-    population_size = 30
-    pixels_percentage_to_change = 0.5
+    population_size = 100
+    pixels_percentage_to_change = 1
     max_generations_count = 1000
     population_percentage_to_keep = 0.2
     mutation_prob = 0.5
